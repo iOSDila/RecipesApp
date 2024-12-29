7dce0d8c63fedcf8c70a074296641d069bf33cfa
iOSDila <pubududilshan97@gmail.com>
2024-12-30 00:10:50 +0530

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Pubudu Dilshan on 2024-12-30.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesAppTests/RecipesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppTests.swift
+//  RecipesAppTests
+//
+//  Created by Pubudu Dilshan on 2024-12-30.
+//
+
+import Testing
+@testable import RecipesApp
+
+struct RecipesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
